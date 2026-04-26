6a0ad3c65346a27070c01cb781fb3c0cfac925ce
koralle <koralle+github@koralle-mgmg.com>
2026-04-26 23:48:44 +0900

    feat: setup

---
+++ packages/lib/oxfmt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  sortImports: true,
+  sortPackageJson: true,
+})
